abcb935330adddcc85906dde54ebe0a8be2feefa
pranaykolhe07 <pranaykolhe07@gmail.com>
2024-07-31 18:24:36 +0530

    01-variableAssign.js
    01-variableAssign.js

---
+++ 01-variableAssign.js
@@ -0,0 +1,39 @@
+console.log("================ 1 ================");
+//declaration
+var myFullName;
+console.log("My Full Name before initialization: ",myFullName);
+
+//initialization
+myFullName = "Pranay Kolhe";
+console.log("My First and Last name after initialization: ",myFullName);
+
+//updation
+myFullName = "Pranay Dhanraj Kolhe";
+console.log("After updating my full name is: ",myFullName);
+
+console.log("================ 2 ================");
+var myLove;
+myLove = "I Love only JavaScript";
+console.log(myLove);
+
+console.log("================ 3 ================");
+var firstName = "Pranay";
+console.log("My First Name : ",firstName);
+
+var lastName = "Kolhe";
+console.log("My Last Name: ",lastName);
+
+var age = 24 ;
+console.log("My Age: ",age);
+
+var address = "Hinganghat, Wardha.";
+console.log("My Address: ",address);
+
+var pinCode = 442301;
+console.log("Pin Code: ",pinCode);
+
+var gender = "male";
+console.log("Gender: ",gender);
+
+var isMarried = false;
+console.log("Is Married: ",isMarried);
