776c57cca5746043d1ef5739266dda2478913b44
pranaykolhe07 <pranaykolhe07@gmail.com>
2024-07-31 07:54:12 +0530

    JavaScript Variables
    JavaScript Variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,6 @@
+console.log('Pranay Kolhe');
+console.log('My City name is : Hinganghat.');
+console.log('My Best-Friend name is : Abhishek.');
+console.log('My Country name is : India.');
+console.log('My School name is : Mahesh Gyanpeeth High School, Hinganghat, Wardha.');
+console.log('My College name is : DYPIMED, Talegaon, Pune.');
