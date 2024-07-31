1f3a8c9eb7779ee151fb4dc93357832f24e62b3c
pranaykolhe07 <pranaykolhe07@gmail.com>
2024-07-31 18:50:10 +0530

    02-typeOf.js
    02-typeOf.js

---
+++ 02-typeOf.js
@@ -0,0 +1,34 @@
+console.log("============= 1 ===============");
+var bankName;
+console.log("Bank Name is:", bankName);
+bankName = "SBI Bank";
+console.log("Bank Name is: ",bankName);
+var bankNameTypeOf = typeof bankName;
+console.log("DataType used: ", bankNameTypeOf);
+
+console.log("============= 2 ===============");
+var myMarks = "81%";
+console.log("My Marks Are: ", myMarks);
+var marksTypeOf = typeof myMarks;
+console.log("DataType used: ", marksTypeOf);
+
+console.log("============= 3 ===============");
+var isWorking;
+console.log("My Working status: ", isWorking);
+isWorking = false;
+console.log("My Working status: ", isWorking);
+var workingTypeOf = typeof isWorking;
+console.log("DataType used: ", workingTypeOf);
+
+console.log("============= 4 ===============");
+console.log("============= a ===============");
+var totalCount = "Hundred and Seven";
+console.log("Total Count is: ", totalCount);
+var countTypeOf = typeof totalCount;
+console.log("DataType used: ", countTypeOf);
+
+console.log("============= b ===============");
+var totalCount = 107;
+console.log("Total Count is: ", totalCount);
+var countTypeOf = typeof totalCount;
+console.log("DataType used: ", countTypeOf);
