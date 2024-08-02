bbf64daeb378c79ffd8c48e65cbfc99d44476ebd
pranaykolhe07 <pranaykolhe07@gmail.com>
2024-08-02 17:35:22 +0530

    03-funAssig01.js
    03-funAssig01.js

---
+++ 03-funAssig01.js
@@ -0,0 +1,32 @@
+console.log("===============Assignment 01===============");
+console.log("=============== .1. ===============");
+
+function quote1(){
+    console.log("-> 'If You Don't Take Risks, You Can't Create a Future.' ");
+    
+}
+quote1();
+
+function quote2(){
+    console.log("-> 'No matter how hard or impossible it is, never loose sight of your goal.' ");
+    
+}
+quote2();
+
+console.log("=============== .2. ===============");
+
+function personalDetails(firstName,lastName,collegeName){
+    console.log("My First Name is: ",firstName);
+    console.log("My Last Name is: ",lastName);
+    console.log("My College Name is: ",collegeName);
+}
+personalDetails("Pranay","Kolhe","DYPIMED, Pune.");
+
+console.log("=============== .3. ===============");
+
+function addThreeValues(a1,a2,a3){
+    var result = a1 + a2 + a3;
+    console.log("Result is: ",result);
+}
+addThreeValues(10.23,600,40);
+addThreeValues("Hello ","Good ","Morning");
