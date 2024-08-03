dc69704b0f583ebb4b5ecb2fd98d75421c8e82c7
pranaykolhe07 <pranaykolhe07@gmail.com>
2024-08-03 11:05:35 +0530

    03-stringBasicAssig01.js
    03-stringBasicAssig01.js

---
+++ 03-stringBasicAssig01.js
@@ -0,0 +1,15 @@
+function stringBasics(){
+    console.log(`My dream company is "Nvidia".`);
+    var hobby1 = "Painting";
+    var hobby2 = "Sketching";
+    var hobby3 = "Programming"
+
+    var hobbiesLength = hobby1.length + hobby2.length + hobby3.length;
+
+    console.log(`My Hobbies are: ${hobby1},${hobby2},${hobby3}`);
+
+    console.log(`Total number of characters: ${hobbiesLength}`);
+    
+    
+}
+stringBasics();
