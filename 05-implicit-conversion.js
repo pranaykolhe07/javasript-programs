086922187ffb612fd84355080d88fd74fa16d9c8
pranaykolhe07 <pranaykolhe07@gmail.com>
2024-08-07 22:42:36 +0530

    05-implicit-conversion.js
    05-implicit-conversion.js

---
+++ 05-implicit-conversion.js
@@ -0,0 +1,14 @@
+// numeric string used with + gives type
+let result;
+
+result = '3' + 2 ;
+console.log(`Result is: ${result}`);
+
+result = '3' + true ;
+console.log(`Result is: ${result}`);
+
+result = '3' + undefined ;
+console.log(`Result is: ${result}`);
+
+result = '3' + null ;
+console.log(`Result is: ${result}`);
