4ced170a1155bc354801cd28cbebb9982caf917a
pranaykolhe07 <pranaykolhe07@gmail.com>
2024-08-10 15:11:54 +0530

    06-for-assignment01.js
    06-for-assignment01.js

---
+++ 06-for-assignment01.js
@@ -0,0 +1,37 @@
+console.log(`--------> Assignment: Using "for" loop. <--------`);
+
+
+console.log(`--->1.Print numbers from 5 to 15 by incrementing 1.`);
+for (let i = 5; i <= 15; i++) {
+    console.log(i);       
+}
+
+console.log(`--->2.Print numbers from 50 to 40, decrementing by 1.`);
+for (let i = 50; i >= 40; i--) {
+    console.log(i);       
+}
+
+console.log(`--->3.Find first 15 odd numbers.`);
+for (let i = 1; i <= 29 ; i = i + 2) {
+    console.log(i);      
+}
+
+console.log(`--->4.Find first 10 even numbers.`);
+for (let i = 2; i <= 20; i = i + 2) {
+    console.log(i);       
+}
+
+console.log(`--->5.Print table of 5.`);
+for (let i = 5; i <= 50; i = i + 5) {
+    console.log(i);       
+}
+
+console.log(`--->6.Print table of 10.`);
+for (let i = 10; i <= 100; i = i + 10) {
+    console.log(i);       
+}
+
+console.log(`--->7.Print table of 10 in reverse order.`);
+for (let i = 100; i >= 10; i = i - 10) {
+    console.log(i);       
+}
